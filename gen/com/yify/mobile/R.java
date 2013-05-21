b935bdd78f243998e0631b2eff909921eb4d7dfe
jacklaaa89 <jacktimblin@gmail.com>
2013-05-21 16:08:11 +0100

    R is autogenerated by the IDE as a link between the XML and JAVA files.

--- gen/com/yify/mobile/R.java
+++ gen/com/yify/mobile/R.java
@@ -8,61 +8,263 @@
 package com.yify.mobile;
 
 public final class R {
+    public static final class anim {
+        public static final int android_back_leave=0x7f040000;
+        public static final int animation_back_enter=0x7f040001;
+        public static final int animation_enter=0x7f040002;
+        public static final int animation_leave=0x7f040003;
+        public static final int slide_left_in=0x7f040004;
+        public static final int slide_left_out=0x7f040005;
+        public static final int slide_right_in=0x7f040006;
+        public static final int slide_right_out=0x7f040007;
+    }
+    public static final class array {
+        public static final int action_list=0x7f060000;
+    }
     public static final class attr {
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int actionbarCompatItemHomeStyle=0x7f010002;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int actionbarCompatItemStyle=0x7f010001;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int actionbarCompatProgressIndicatorStyle=0x7f010003;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int actionbarCompatTitleStyle=0x7f010000;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int borderDrawable=0x7f010005;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int maskDrawable=0x7f010004;
+    }
+    public static final class color {
+        public static final int actionbar_title_color=0x7f070000;
+        public static final int text_white=0x7f070001;
     }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
-         */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int actionbar_compat_button_home_width=0x7f080002;
+        public static final int actionbar_compat_button_width=0x7f080001;
+        public static final int actionbar_compat_height=0x7f080000;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int about_button=0x7f020000;
+        public static final int about_button_normal=0x7f020001;
+        public static final int about_button_selected=0x7f020002;
+        public static final int actionbar_compat_item=0x7f020003;
+        public static final int actionbar_compat_item_focused=0x7f020004;
+        public static final int actionbar_compat_item_pressed=0x7f020005;
+        public static final int actionbar_shadow=0x7f020006;
+        public static final int button_base=0x7f020007;
+        public static final int contact_button=0x7f020008;
+        public static final int contact_button_normal=0x7f020009;
+        public static final int contact_button_selected=0x7f02000a;
+        public static final int default_product=0x7f02000b;
+        public static final int default_tweet=0x7f02000c;
+        public static final int headtest3=0x7f02000d;
+        public static final int ic_action_refresh_white=0x7f02000e;
+        public static final int ic_action_search=0x7f02000f;
+        public static final int ic_action_share=0x7f020010;
+        public static final int ic_home=0x7f020011;
+        public static final int ic_launcher=0x7f020012;
+        public static final int ic_menu_share=0x7f020013;
+        public static final int icon_more=0x7f020014;
+        public static final int logo=0x7f020015;
+        public static final int navigation_refresh=0x7f020016;
+        public static final int partfinder=0x7f020017;
+        public static final int searchcategory=0x7f020018;
+        public static final int twitter_feed_background=0x7f020019;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int actionbar_compat=0x7f090000;
+        public static final int actionbar_compat_item_refresh=0x7f090003;
+        public static final int actionbar_compat_item_refresh_progress=0x7f090002;
+        public static final int actionbar_compat_title=0x7f090001;
+        public static final int err_message=0x7f09000c;
+        public static final int image=0x7f090006;
+        public static final int main_loading_bar=0x7f09000b;
+        public static final int main_loading_view=0x7f09000a;
+        public static final int main_view_state=0x7f090009;
+        public static final int menu_home=0x7f090017;
+        public static final int menu_login=0x7f090018;
+        public static final int menu_refresh=0x7f090004;
+        public static final int menu_search=0x7f090016;
+        public static final int menu_settings=0x7f09001a;
+        public static final int menu_share=0x7f090019;
+        public static final int pager=0x7f090005;
+        public static final int refresh_twitter=0x7f09001c;
+        public static final int results_text=0x7f090010;
+        public static final int search_listView=0x7f090011;
+        public static final int search_listrow=0x7f090012;
+        public static final int second_image=0x7f090013;
+        public static final int second_text=0x7f090014;
+        public static final int show_twitter=0x7f09001b;
+        public static final int text=0x7f090007;
+        public static final int text2=0x7f090008;
+        public static final int twitter_feed_list=0x7f090015;
+        public static final int upcoming_linear_list=0x7f09000d;
+        public static final int upcoming_list=0x7f09000f;
+        public static final int upcoming_text=0x7f09000e;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int about_pager=0x7f030000;
+        public static final int actionbar_compat=0x7f030001;
+        public static final int actionbar_indeterminate_progress=0x7f030002;
+        public static final int cookies_fragment=0x7f030003;
+        public static final int faq_fragment=0x7f030004;
+        public static final int item=0x7f030005;
+        public static final int main=0x7f030006;
+        public static final int privacy_fragment=0x7f030007;
+        public static final int returns_fragment=0x7f030008;
+        public static final int search=0x7f030009;
+        public static final int search_listrow=0x7f03000a;
+        public static final int terms_fragment=0x7f03000b;
+        public static final int twitter_feed_row=0x7f03000c;
+        public static final int twitter_list=0x7f03000d;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f0c0000;
+        public static final int twitter_popup=0x7f0c0001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int about=0x7f0a0009;
+        public static final int alternate_title=0x7f0a0001;
+        public static final int app_name=0x7f0a0000;
+        public static final int clear=0x7f0a0008;
+        public static final int menu_basket=0x7f0a000c;
+        public static final int menu_contact=0x7f0a000f;
+        public static final int menu_expand=0x7f0a0006;
+        public static final int menu_finder=0x7f0a000b;
+        public static final int menu_home=0x7f0a000a;
+        public static final int menu_refresh=0x7f0a0003;
+        public static final int menu_search=0x7f0a0004;
+        public static final int menu_settings=0x7f0a000e;
+        public static final int menu_share=0x7f0a0005;
+        public static final int menu_twitter=0x7f0a000d;
+        public static final int refresh_twitter=0x7f0a0011;
+        public static final int search_hint=0x7f0a0007;
+        public static final int show_twitter=0x7f0a0010;
+        public static final int toggle_title=0x7f0a0002;
     }
     public static final class style {
-        /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
-         */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
+        public static final int ActionBar=0x7f0b0008;
+        public static final int ActionBarCompat=0x7f0b0001;
+        public static final int ActionBarCompatHomeItem=0x7f0b0007;
+        public static final int ActionBarCompatItem=0x7f0b0006;
+        public static final int ActionBarCompatItemBase=0x7f0b0002;
+        public static final int ActionBarCompatProgressIndicator=0x7f0b0003;
+        public static final int ActionBarCompatTitle=0x7f0b0005;
+        public static final int ActionBarCompatTitleBase=0x7f0b0004;
+        public static final int ActionBarTitle=0x7f0b000a;
+        public static final int AppTheme=0x7f0b0000;
+        public static final int myDropDownListView=0x7f0b0009;
     }
+    public static final class xml {
+        public static final int searchable=0x7f050000;
+    }
+    public static final class styleable {
+        /** Attributes that can be used with a AppTheme.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #AppTheme_actionbarCompatItemHomeStyle com.yify.mobile:actionbarCompatItemHomeStyle}</code></td><td></td></tr>
+           <tr><td><code>{@link #AppTheme_actionbarCompatItemStyle com.yify.mobile:actionbarCompatItemStyle}</code></td><td></td></tr>
+           <tr><td><code>{@link #AppTheme_actionbarCompatProgressIndicatorStyle com.yify.mobile:actionbarCompatProgressIndicatorStyle}</code></td><td></td></tr>
+           <tr><td><code>{@link #AppTheme_actionbarCompatTitleStyle com.yify.mobile:actionbarCompatTitleStyle}</code></td><td></td></tr>
+           </table>
+           @see #AppTheme_actionbarCompatItemHomeStyle
+           @see #AppTheme_actionbarCompatItemStyle
+           @see #AppTheme_actionbarCompatProgressIndicatorStyle
+           @see #AppTheme_actionbarCompatTitleStyle
+         */
+        public static final int[] AppTheme = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.yify.mobile.R.attr#actionbarCompatItemHomeStyle}
+          attribute's value can be found in the {@link #AppTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:actionbarCompatItemHomeStyle
+        */
+        public static final int AppTheme_actionbarCompatItemHomeStyle = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.yify.mobile.R.attr#actionbarCompatItemStyle}
+          attribute's value can be found in the {@link #AppTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:actionbarCompatItemStyle
+        */
+        public static final int AppTheme_actionbarCompatItemStyle = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.yify.mobile.R.attr#actionbarCompatProgressIndicatorStyle}
+          attribute's value can be found in the {@link #AppTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:actionbarCompatProgressIndicatorStyle
+        */
+        public static final int AppTheme_actionbarCompatProgressIndicatorStyle = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.yify.mobile.R.attr#actionbarCompatTitleStyle}
+          attribute's value can be found in the {@link #AppTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:actionbarCompatTitleStyle
+        */
+        public static final int AppTheme_actionbarCompatTitleStyle = 0;
+        /** Attributes that can be used with a BezelImageView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #BezelImageView_borderDrawable com.yify.mobile:borderDrawable}</code></td><td></td></tr>
+           <tr><td><code>{@link #BezelImageView_maskDrawable com.yify.mobile:maskDrawable}</code></td><td></td></tr>
+           </table>
+           @see #BezelImageView_borderDrawable
+           @see #BezelImageView_maskDrawable
+         */
+        public static final int[] BezelImageView = {
+            0x7f010004, 0x7f010005
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.yify.mobile.R.attr#borderDrawable}
+          attribute's value can be found in the {@link #BezelImageView} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:borderDrawable
+        */
+        public static final int BezelImageView_borderDrawable = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.yify.mobile.R.attr#maskDrawable}
+          attribute's value can be found in the {@link #BezelImageView} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:maskDrawable
+        */
+        public static final int BezelImageView_maskDrawable = 0;
+    };
 }
