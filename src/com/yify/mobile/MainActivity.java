29c3f31b6990e35d3ce564ce239cd96e0f95e5db
jacklaaa89 <jacktimblin@gmail.com>
2013-05-22 16:24:14 +0100

    Added network connection checks Added network check when this activity starts any other activity, and halts execution if there is no connection.

--- src/com/yify/mobile/MainActivity.java
+++ src/com/yify/mobile/MainActivity.java
@@ -12,10 +12,12 @@ import com.yify.object.UpcomingObject;
 import android.widget.ListView;
 import android.os.Bundle;
 import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -24,6 +26,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
+import android.widget.SearchView.OnSuggestionListener;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
@@ -35,14 +38,17 @@ public class MainActivity extends ActionBarActivity {
 	private Menu mainMenu = null;
 	private ViewFlipper flipper;
 	private ListView listView;
+	private ActionBar actionBar;
+	private ConnectivityDetector detector;
+	private SearchView searchView; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+		actionBar = getActionBar();
 		flipper = (ViewFlipper) findViewById(R.id.main_view_state);
-		
+		detector = new ConnectivityDetector(this);
 		
 	}
 	
@@ -50,6 +56,12 @@ public class MainActivity extends ActionBarActivity {
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		outState.putInt("SAVED_INDEX", 1);
+	}
+	
+	@Override
+	public boolean onSearchRequested() {
+		
+		return false;
 	}
 
 	@Override
@@ -60,12 +72,12 @@ public class MainActivity extends ActionBarActivity {
 		
 		//set up the default search service.
 		SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-		final SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+		searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
 		searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-		
 		//call superclass constructor.
 		boolean end = super.onCreateOptionsMenu(menu);
 		mainMenu.findItem(R.id.menu_home).setVisible(false); /* <-- already on home. */
+		mainMenu.findItem(R.id.menu_filter).setVisible(false);
 		
 		//execute background task to grab upcoming movies.
 		getActionBarHelper().setRefreshActionItemState(true);
@@ -81,6 +93,18 @@ public class MainActivity extends ActionBarActivity {
 	}
 	
 	@Override
+	public void startActivity(Intent intent) {
+		if(!detector.isConnectionAvailable()) {
+			Toast.makeText(this, "Network not available, Please make sure you are connected to a network.", Toast.LENGTH_SHORT).show();
+			if(Intent.ACTION_SEARCH.equals(intent.getAction())) {
+				searchView.setIconified(true);
+			}
+			return;
+		}
+		super.startActivity(intent);
+	}
+	
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		
 		switch(item.getItemId()) {
@@ -90,11 +114,22 @@ public class MainActivity extends ActionBarActivity {
 				view.setVisibility(View.GONE);
 				loading.setVisibility(View.VISIBLE);
 				flipper.setDisplayedChild(0);
-				Toast.makeText(this, "Refreshing Upcoming Movie Feed...", Toast.LENGTH_SHORT).show();
+				if(!detector.isConnectionAvailable()) {
+					Toast.makeText(this, "No Network Connection....", Toast.LENGTH_SHORT).show();
+					
+				} else  {
+					Toast.makeText(this, "Refreshing Upcoming Movie Feed...", Toast.LENGTH_SHORT).show();
+				}
 				getActionBarHelper().setRefreshActionItemState(true);
 				
 				new getUpcoming().execute("");
 				break;
+			case R.id.menu_search:
+				Toast.makeText(this, "Search Pressed", Toast.LENGTH_SHORT).show();
+				if(!detector.isConnectionAvailable()) {
+					return true;
+				}
+				return false;
 		}
 		
 		return super.onOptionsItemSelected(item);
@@ -141,7 +176,7 @@ public class MainActivity extends ActionBarActivity {
 			
 			
 			MainActivity.this.listView.setFastScrollEnabled(true);
-			final ProductAdapter adapter = new ProductAdapter(MainActivity.this, response, false);
+			final ProductAdapter<UpcomingObject> adapter = new ProductAdapter<UpcomingObject>(MainActivity.this, response, false);
 			MainActivity.this.listView.setAdapter(adapter);
 			MainActivity.this.listView.setOnItemClickListener(new OnItemClickListener() {
 
