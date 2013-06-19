15f172831259ef68863bac3db9ca035c0a62cfc0
jacklaaa89 <jacktimblin@gmail.com>
2013-06-19 07:56:25 +0100

    Updated the replyDialog to return the correct view to the activity that created the dialog. Also removed checkbox.

--- src/com/yify/object/ReplyDialog.java
+++ src/com/yify/object/ReplyDialog.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class ReplyDialog extends DialogFragment {
@@ -42,31 +43,63 @@ public class ReplyDialog extends DialogFragment {
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		
 		LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		final View v = inflater.inflate(R.layout.login_dialog, null);
+		final View v = inflater.inflate(R.layout.reply_dialog, null);
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		
-		v.findViewById(R.id.password).setVisibility(View.GONE);
-		v.findViewById(R.id.signinbutton).setVisibility(View.GONE);
+		Button cancel = (Button) v.findViewById(R.id.cancel);
+		final Button reply = (Button) v.findViewById(R.id.reply);
+		final EditText message = (EditText) v.findViewById(R.id.editText1);
 		
-		builder.setTitle("Leave Comment").setView(v)
-		.setPositiveButton("Reply", new DialogInterface.OnClickListener() {
-			
+		cancel.setOnClickListener(new OnClickListener() {
+
 			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				
-				listener.onReplyPressed(ReplyDialog.this, v);
-				
-			}
-		})
-		.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
+			public void onClick(View arg0) {
 				
 				listener.onCancelPressed(ReplyDialog.this);
 				
 			}
+			
 		});
+		
+		TextWatcher watcher = new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				
+				if(message.getText().toString().length() != 0) {
+					
+					reply.setEnabled(true);
+					
+				} else {
+					reply.setEnabled(false);
+				}
+				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {}
+			
+		};
+		
+		message.addTextChangedListener(watcher);
+		
+		reply.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View view) {
+				
+				listener.onReplyPressed(ReplyDialog.this, v);
+				
+			}
+			
+		});
+		
+		builder.setTitle("Leave Comment").setView(v);
 		
 		return builder.create();
 	}
